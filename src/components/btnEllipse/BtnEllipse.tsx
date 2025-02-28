0873c43c01373e6abc975b74797e49b9d65198c3
Dima163163 <khlopovdima@gmail.com>
2025-02-28 14:51:23 +0400

    hover btn

--- src/components/btnEllipse/BtnEllipse.tsx
+++ src/components/btnEllipse/BtnEllipse.tsx
@@ -4,10 +4,16 @@ import styled from 'styled-components';
 export const BtnEllipse = styled.button`
   border-radius: 100%;
   background: linear-gradient(315deg, #e2a300 0%, #e29500 100%);
+  border: 2px solid transparent;
   min-width: 60px;
   height: 60px;
   display: flex;
   justify-content: center;
   align-items: center;
   cursor: pointer;
+
+  &:hover{
+    background: transparent;
+    border-color: #e29500;
+  }
 `
--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -5,11 +5,17 @@ export const Button = styled.button`
   height: 47px;
   padding: 14px 38px 13px 41px;
   border-radius: 6px;
+  border: 2px solid transparent;
   box-shadow: 0 2px 13px 0 rgba(226, 158, 0, 0.48);
-  background: linear-gradient(315deg, #e2a300 0%, #e29500 100%);
+  background-color: linear-gradient(315deg, #e2a300 0%, #e29500 100%);
   text-transform: uppercase;
   font-family: 'NEXT ART', sans-serif;
   font-weight: 700;
   font-size: 18px;
   color: #000;
+  &:hover {
+    background: none;
+    border-color: #e2a300;
+    color: #e2a300;
+  }
 `
